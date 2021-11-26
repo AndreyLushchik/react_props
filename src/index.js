992360d3b3d268ae059b42b0980635cc9ae10e33
AndreyLushchik <redbull199556@gmail.com>
2021-11-26 16:24:23 +0300

    modifying files to create squares

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import Square from "./App";
+import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<Square className="big-square" label="BIG Square" />
+		<Square className="less-square" label="A less Square" />
+		<Square className="small-square" label="The smallest one" />
+	</React.StrictMode>,
+	document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
